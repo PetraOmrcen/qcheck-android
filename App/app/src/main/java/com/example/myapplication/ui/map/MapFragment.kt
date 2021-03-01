0f5ac6f4b1e9df471cd71bab9b574495e1d28194
Tin Oroz <tinoroz@gmail.com>
2021-03-01 16:24:58 +0100

    Adding bottom navbar

--- App/app/src/main/java/com/example/myapplication/ui/map/MapFragment.kt
+++ App/app/src/main/java/com/example/myapplication/ui/map/MapFragment.kt
@@ -1,7 +1,9 @@
-package com.example.myapplication
+package com.example.myapplication.ui.map
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,13 +12,13 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapFragment : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
+        setContentView(R.layout.fragment_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
